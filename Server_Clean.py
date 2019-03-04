b9e76b953e74ab752412eab9733f8770e0220a1b
Jamison Bunge <JamisonBunge@outlook.com>
2019-03-04 18:43:43 -0500

    Replaces files that are our of date

--- Server_Clean.py
+++ Server_Clean.py
@@ -9,18 +9,12 @@ from wsgiref.handlers import format_date_time
 from datetime import datetime
 from time import mktime
 #import ordered_set
-cachedFiles = dict()
 
-#Global Constants: not my code, need to modify
-MAX_PENDING = 5
+cachedFiles = dict()
 DIRECTORY_ROOT = "Cache"
-WORKING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 BUF_SIZE = 1024
-EXIT_CODE = True
-STATUS_LOOKUP = {200: "OK", 201: "Created", 202: "Accepted", 204: "No Content",
-                 301: "Moved Permanently", 302: "Moved Temporarily", 304: "Not Modified",
-                 400: "Bad Request", 401: "Unauthorized", 403: "Forbidden", 404: "Not Found",
-                 500: "Internal Server Error", 501: "Not Implemented", 502: "Bad Gateway", 503: "Service Unavailable"}
+WORKING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+
 """
 Internal Datastructure Format
 -----------------------------
@@ -39,11 +33,10 @@ VALUE -> A tuple that stores information used to see if the file is out of date
         4 -> host for the GET request
         5 -> file for the GET request 
 
-
-
-
 """
 
+
+#def proc():
 
 
 def main():
@@ -55,7 +48,7 @@ def main():
     print(WORKING_DIRECTORY)
 
     #Create a thread that will allow the user to kill the server from the terminal.
-    #listenForUser = Thread(target=killServer, args=())
+    #listenForUser = Thread(target=killServer, args=(hello))
     #listenForUser.start()
 
     while True:
@@ -68,7 +61,7 @@ def main():
 
         message = clientSocket.recv(BUF_SIZE)   
 
-        print(message)
+        #print(message)
         # Extract the filename from the given message
         #print(message.split()[1])
         filename = message.split()[1].partition("/")[2]
@@ -84,18 +77,23 @@ def main():
             fileToCheckTime = fileToRead 
             fileToRead = "/" + fileToRead
             print(fileToRead)
-            
+            print("A")
             ############################################################
 
             if fileToCheckTime in cachedFiles.keys():
-                
+                print("B")
+                print(cachedFiles.get(fileToCheckTime))
                 f = open(fileToRead[1:], "r")
+                print("C")
                 tupleResponse = cachedFiles.get(fileToCheckTime)
+                print("D")
+
                 #I am parsing all of the relevent information into seperate variables
-                #This is not needed, I'm just doing it for readablity
+                #This is not needed, I'm just doing it for readablity'
+
                 savedDateLastMod =tupleResponse[0]
                 savedDateCached = tupleResponse[1]
-                sxavedActivePath = tupleResponse[2]
+                savedActivePath = tupleResponse[2]
                 savedHostn = tupleResponse[3]
                 savedHostNameForRequest = tupleResponse[4]
                 savedRelativePath = tupleResponse[5]
@@ -104,48 +102,76 @@ def main():
                 socketToCheckIfModded =  socket(AF_INET,SOCK_STREAM)
                 socketToCheckIfModded.connect((savedHostn,80))
 
-                fileob = c.makefile('r', 0) 
+                fileob = socketToCheckIfModded.makefile('r', 0) 
+                print("E")
                    
         
                 #Pass the arguments into the write lines using string format
                 fileob.write("GET {object} HTTP/1.0\r\n".format(object=savedRelativePath))
+                print("F")
                 fileob.write("Host: {host}\r\n".format(host=savedHostNameForRequest))
+                print("G")
                 fileob.write("If-Modified-Since: {date}\r\n\r\n".format(date=savedDateLastMod))
+                print("H")
+
 
                 #Print the lines
                 print("GET {object} HTTP/1.0".format(object=relativePath))
-                print("Host: {host}\n".format(host=hostNameForRequest))
+                print("Host: {host}".format(host=hostNameForRequest))
                 print("If-Modified-Since: {date}\r\n\r\n".format(date=savedDateLastMod))
 
-                buff = fileobj.readline()
-                print("first line of response: " + buff)
+                print("I")
+
+                b = fileob.readline()
+                
+                s=b
+                s=s.split(" ")
+                responseCode = s[1]
+
+                socketToCheckIfModded.close()
+
+                if responseCode == "304":
+                    print("Code 304: Cache is up to date.")
+                    print("The saved file will be served")
+                elif responseCode == "200":
+                    print("Code 200: The Cache is either out of date or a 304 was not sent when it should have been.")
+                    print("This is ambiguous so the server will delete the cached file and download a new one")
+                    
+                    try:
+                        #os.chdir(savedActivePath)
+                        print("j")
+                        os.remove("." + fileToRead)
+                        print("f")
+                        #os.chdir(WORKING_DIRECTORY + "/" + DIRECTORY_ROOT)
+                        print("d")
+                        print("file removed from disk")
+                    except:
+                        print("file could not be removed")
+
+
+                    del cachedFiles[fileToCheckTime]
+
+                    #TODO: delete from dictionary
+
+                    raise IOError
+                else:
+                    print("Received a response code, {code} that could not be understood by this implementation".format(code=responseCode))
+                    print("The cached file will be served to the browser")
+      
+
+                print(s)
+                print(s[1])
+
+                print("J")
 
                 
-
-                
-                print("THIS: " +tupleResponse[1])
                 
                 
-                
-                
-                
-                print(cachedFiles.get(fileToCheckTime))
-                
-
-
-
 
 
             else:
                 print("Manually throwing IOError")
                 raise IOError
-                
-
-
-
-
-
-
 
 
             ############################################################
@@ -233,17 +259,26 @@ def main():
                    
                     print("3")
 
+
+
+                    now = datetime.now()
+                    stamp = mktime(now.timetuple())
+                    dateLastMod =  format_date_time(stamp)
+
                     # Fill in start.
                     for m in buffer:
+                        if "Last-Modified" in m:
+                            dateLastMod = m.strip().split(' ', 1)[1]
+                            print("HEY ITS ME THE GUY YOUR LOOKING FOR : " + dateLastMod)
                         tmpFile.write(m)
                         clientSocket.send(m)
-                    print("4")
+                    #print("4")
                     
                     tmpFile.close()
                     
           
                     rWriteName = "/" + writeName
-                    f = open(rWriteName[1:], "r")
+                    # f = open(rWriteName[1:], "r")
                     print("5")
 
 
@@ -252,13 +287,13 @@ def main():
                     dateCached =  format_date_time(stamp)
 
                     #
-                    for line in f:
-                        if "Last-Modified" in line:
-                            dateLastMod = line.strip().split(' ', 1)[1]
-                            print(dateLastMod)
-                            break
-                    f.close()
-                    print("6")
+                    # for line in f:
+                    #     if "Last-Modified" in line:
+                    #         dateLastMod = line.strip().split(' ', 1)[1]
+                    #         print(dateLastMod)
+                    #         break
+                    # f.close()
+                    #print("6")
 
                     if writeName not in cachedFiles.keys():
                         now = datetime.now()
@@ -266,7 +301,7 @@ def main():
                         dateCached =  format_date_time(stamp)
                         
                         cachedFiles.update({writeName : (dateLastMod,dateCached,activePath,hostn,hostNameForRequest,relativePath)})
-                        #{  writename for cache                    }
+
 
 
                 except:
