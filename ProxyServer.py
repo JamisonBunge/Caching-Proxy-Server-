03d855c4dd02b255fcf2ad48197903d60ab1b216
Jamison Bunge <JamisonBunge@outlook.com>
2019-03-04 23:11:15 -0500

    Final Product

--- ProxyServer.py
+++ ProxyServer.py
@@ -1,183 +1,312 @@
 #!/usr/bin/python2.7
 
+"""
+Internal Data-Structure Format
+-----------------------------
+**This information is explained in the READ_ME**
+
+The cache information is stored in a dictionary called cachedFiles
+
+    KEY ->      The filename formated so it can be written to the hard disk
+    VALUE ->    A tuple that stores information used to see if the file is out of date 
+
+    TUPLE FIELDS ->     0 -> Last-Modifed parameter from the the http response
+                        NOTE: if this this was not found in the header then this value will be the time the request was made
+                        1 -> The date the get reqeust was made / the date the saved file was created 
+                        2 -> The path to the cached file within the created hierarchy 
+                        3 -> hostname 
+                        4 -> host for the GET request
+                        5 -> file for the GET request 
+"""
+
+#IMPORTS
 from socket import *
 import sys
 import os
 from threading import Thread,Lock
+from collections import namedtuple
+from wsgiref.handlers import format_date_time
+from datetime import datetime
+from time import mktime
 
-#Global Constants: not my code, need to modify
-MAX_PENDING = 5
-MAX_BUFSIZE = 4096
-EXIT_CODE = True
-STATUS_LOOKUP = {200: "OK", 201: "Created", 202: "Accepted", 204: "No Content",
-                 301: "Moved Permanently", 302: "Moved Temporarily", 304: "Not Modified",
-                 400: "Bad Request", 401: "Unauthorized", 403: "Forbidden", 404: "Not Found",
-                 500: "Internal Server Error", 501: "Not Implemented", 502: "Bad Gateway", 503: "Service Unavailable"}
+#GLOBAL VARS
+cachedFiles = dict()
+DIRECTORY_ROOT = "Cache"
+BUF_SIZE = 1024
+WORKING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
-def killServer():
-    print("To close the server type \"exit\" at any time.")
-    while 1:
-        if raw_input("")=="exit":
-            sys.exit("The server has beeen stopped.")
 
 def main():
-   
-    makeDirectory()
+    #CREATE FILE HIERARCHY
+    makeDirectory(DIRECTORY_ROOT)
+    os.chdir(DIRECTORY_ROOT)
+
+    #CREATE SERVER SOCKET
     serverSocket = establishServer()
 
-    listenForUser = Thread(target=killServer, args=())
-    listenForUser.start()
+    while True:
 
-    while 1:
-        # Start receiving data from the client
+        #WAIT TO RECV DATA FROM THE CLIENT
         print('Ready to serve...')
-        tcpCliSock, addr = serverSocket.accept()
+        clientSocket, addr = serverSocket.accept()
         print('Received a connection from:', addr)
 
-        ########################################################################################
-        message = tcpCliSock.recv(1024)
-        ########################################################################################
-        
-        print(message)
+
+        message = clientSocket.recv(BUF_SIZE)   
+
+        #print(message)
         # Extract the filename from the given message
-        print(message.split()[1])
+        #print(message.split()[1])
         filename = message.split()[1].partition("/")[2]
-        #print(filename)
+        print(filename)
         fileExist = "false"
         filetouse = "/" + filename
-        print("fileName: " + filetouse )
-    #print(filetouse)
-        try:
-            # Check wether the file exist in the cache
-            print("INSIDE")
-            f = open(filetouse[1:], "r")
-            print("OPENED FILE")
+ 
+        # CHECK TO SEE IF THE FILE EXISTS IN THE CACHE
+        try:  
+           
+            
+            #FORMATING STRINGS FOR USE BELOW
+            fileToRead = filename.replace("/",".")
+            fileToCheckTime = fileToRead 
+            fileToRead = "/" + fileToRead
+
+            #CHECK TO SEE IF THE FILE HAS BEEN CACHED THIS SESSION
+            if fileToCheckTime in cachedFiles.keys():
+
+                #OPEN THE FILE 
+                #IF THE FILE DOES NOT EXIST AN IO-EXCEPTION WILL RAISE AND THE FILE WITH BE FETCHED
+                f = open(fileToRead[1:], "r")
+                fileExist = "true"
+
+                #TUPLE PARAMS AS NAMED VARS FOR READABLITY 
+                tupleResponse = cachedFiles.get(fileToCheckTime)
+
+                savedDateLastMod =tupleResponse[0]
+                savedDateCached = tupleResponse[1]
+                savedActivePath = tupleResponse[2]
+                savedHostn = tupleResponse[3]
+                savedHostNameForRequest = tupleResponse[4]
+                savedRelativePath = tupleResponse[5]
+
+                #CHECK TO SEE IF MODIFED
+                socketToCheckIfModded =  socket(AF_INET,SOCK_STREAM)
+                socketToCheckIfModded.connect((savedHostn,80))
+
+                #CREATE MAKE FILE FOR SEND/RECV 
+                fileob = socketToCheckIfModded.makefile('r', 0) 
+                   
+                #WRITE TO THE MAKEFILE TO SEND THE HTTP REQUEST TO THE WEB SERVER
+                fileob.write("GET {object} HTTP/1.0\r\n".format(object=savedRelativePath))
+                fileob.write("Host: {host}\r\n".format(host=savedHostNameForRequest))
+                fileob.write("If-Modified-Since: {date}\r\n\r\n".format(date=savedDateLastMod))
+
+                #PRINT LINES TO THE TERMINAL 
+                print("GET {object} HTTP/1.0".format(object=relativePath))
+                print("Host: {host}".format(host=hostNameForRequest))
+                print("If-Modified-Since: {date}\r\n\r\n".format(date=savedDateLastMod))
+
+                #PARSE RESPONSE CODE FROM RESPONSE
+                b = fileob.readline()
+                responseCode=b.split(" ")[1]
+
+                #CLOSE THE SOCKET
+                socketToCheckIfModded.close()
+
+                #DETERMINE ACTION BASED ON RESPONSE CODE
+                if responseCode == "304":
+                    
+                    #THE CACHE IS UP TO DATE, STAY IN THE TRY BLOCK AND SEND THE FILE
+                    print("Code 304: Cache is up to date.")
+                    print("The saved file will be served")
+
+                elif responseCode == "200":
+
+                    #THIS IS AN AMBIGOUS RESPONSE. SOME WEBSITES ARE SENDING THIS EVEN WHEN THEY ARE UP TO DATE
+                    #BECAUSE IT MAY BE OUT OF DATE, DELETE THE CACHED FILE AND FETCH A NEW VERSION 
+                    print("Code 200: The Cache is either out of date or a 304 was not sent when it should have been.")
+                    print("This is ambiguous so the server will delete the cached file and download a new one")
+                    
+                    #TRY AND REMOVE THE FILE
+                    try:
+                        os.remove("." + fileToRead)
+                        print("file removed from disk")
+                    except:
+                        print("file could not be removed")
+
+                    #REMOVE ENTRY FROM THE INTERNAL DATASTRUCT
+                    del cachedFiles[fileToCheckTime]
+
+                    #RAISE AN IOERROR SO A NEW RESPONSE CAN BE FETCHED
+                    fileExist = "false"
+                    raise IOError
+                else:
+
+                    #THIS IMPLEMTATION ONLY HANDLES 200, 404, AND 304 RESPONSES
+                    #ALL OTHER RESPONESES AT THIS STAGE WILL BE TREATED AS 304 AND THE FILE WILL BE SERVED TO THE BROWSER
+                    print("Received a response code, {code} that could not be understood by this implementation".format(code=responseCode))
+                    print("The cached file will be served to the browser")
+
+             
+
+            else:
+                
+                #IF THE FILE WAS NOT CACHED IN THIS SESSION, FETCH IT BY RAISING AN IOERROR
+
+                raise IOError
+
+            #READ THE DATA FROM THE CACHED FILE
             outputdata = f.readlines()
-            fileExist = "true"
-            # ProxyServer finds a cache hit and generates a response message
-            tcpCliSock.send("HTTP/1.0 200 OK\r\n")
-            tcpCliSock.send("Content-Type:text/html\r\n")
-            ########################################################################################
-            # Fill in start.
-            for g in range(0, len(outputdata)):
-                tcpCliSock.send(outputdata[g])
-            # Fill in end.      
-            ########################################################################################
+            
+            #raise IOError #DO NOT KEEP ME IN THIS CODE <---
+
+            #RESPONSE MESSAGE FOR THE CLIENT
+            clientSocket.send("HTTP/1.0 200 OK\r\n")
+            clientSocket.send("Content-Type:text/html\r\n")
+
+            #send data from the file to the client 
+            #SEND DATA FROM THE CACHED FILE
+            for m in outputdata:
+                clientSocket.send(m)
 
             print('Read from cache')
-        # Error handling for file not found in cache
+
+        #ERROR HANDLING FOR THE FILE NOT BEING FOUND IN THE CACHE 
+        #THIS BLOCK IS ALSO RAN WHEN A FILE IS FOUND BUT OUT OF DATE
         except IOError:
             if fileExist == "false":
-                # Create a socket on the proxyserver
-                ########################################################################################
-                c =  socket(AF_INET,SOCK_STREAM)
-                ########################################################################################
-                hostn = filename
-                hostn = filename.replace("www.","",1)
-                #print(hostn)
-                try:           
-                    # Connect to the socket to port 80
-                    # Fill in start.
-                    print("00")
-                    c.connect((hostn,80))
-                    print("88")
-                    # Fill in end.
-                    # Create a temporary file on this socket and ask port 80 for the file requested by the client
-                    fileobj = c.makefile('r', 0) #Instead of using send and recv, we can use makefile
-                    print("1")
-                    #print("GET "+  filename + "HTTP/1.1\r\nHost: www.ee.columbia.edu\r\n\r\n")
-                # print("Host: " + hostn + "\n\r")
-                    
-                                            #GET http://www.ee.columbia.edu/keren-bergman HTTP/1.10\r\n\r\n
-                                            #GET http://www.ee.columbia.edu/keren-bergman HTTP/1.0\r\nHost: www.ee.columbia.edu/keren-bergman\n\n
-
-                    getL = "GET " + "http://" + filename + " HTTP/1.0\r\n\r\n"
-                    hostL = "Host: "+ hostn + "\r\n\r\n"
                 
+                #SOCKET FOR FETCHING DATA FROM A WEBPAGE
+                c =  socket(AF_INET,SOCK_STREAM)
 
+                #GET FILEPATH AFTER THE FIRST SLASH
+                fileNameAsStringArray = filename.partition("/")
 
-                    a = open("getLog.txt", "a+")
-                    a.write(getL)
+                #IMPORTANT INFORMATION IN COMMENT BELOW 
+                """
+                Whenever a new host is going to be connected, we want to save
+                all of the the associated files in a new folder. This way, the cached 
+                files for each website can be grouped together.
+
+                Also, hostn should only be changed when a "www." is detected. This is because all
+                of the request following the first one sent do not include www.websiteName.com and a host
+                name can not be parsed from this GET request alone. Changing the host name only under this condition
+                fixes this problem.
+                """
+
+                #CHECK TO SEE IF THE GET REQUEST INCLDUES A WWW.
+                if "www." in fileNameAsStringArray[0]: 
                     
-                    a.write("BREAK\n")
-                    #a.close()
-                    print("1.1")
+                    #IF IT DOES, MAKE IT HOSTN FOR THE SOCKET CONNECTION
+                    hostn = fileNameAsStringArray[0].replace("www.","",1)
+                    print("HostName: " +hostn)
+                    
+                    #CHANGE DIRECTORY TO OUR LOCAL 'ROOT'
+                    os.chdir(WORKING_DIRECTORY + "/" + DIRECTORY_ROOT)
 
-                    fileobj.write(getL)#Host: " + hostn + "\r\n\r\n")
-                    #fileobj.write(hostL)
-                    #fileobj.write("\n\n")
-                    #fileobj.write("\r\n")
-                    #fileobj.write("Host: " + hostn + "\n\r")
-                    print("2")
-                    # Read the response into buffer
-                    ########################################################################################
-                    # Fill in start.
-                    buff = fileobj.readlines()  
-                    print("2.1")              
-                    # Fill in end.
-                    ########################################################################################
-                    # Create a new file in the cache for the requested file.
-                    # Also send the response in the buffer to client socketand the corresponding file in the cache
-                    #fileN = "./" + filename + ".txt"
-                    tmpFile = open("./" + filename,"wb")
-                # print("./" + filename)
-                    print("3")
-                    ########################################################################################
-                    # Fill in start.
-                    for i in range(0, len(buff)):
-                        a.write(buff[i])
-                        tmpFile.write(buff[i])
-                        tcpCliSock.send(buff[i])
-                    print("4")
-                    #tmpFile.close()
-                    a.close()
-                    # Fill in end.
-                    ########################################################################################
+                    #MAKE A NEW DIRECTORY FOR EVERY HOSTN, THIS WILL KEEP THE FILES GROUPED TOGETHER
+                    makeDirectory(hostn.replace("/","."))
+
+                    #MAKE A COPY OF THE DIRECTORY PATH, THIS WILL BE STORED IN THE INTERNAL DATASTRUCTURE
+                    activePath = WORKING_DIRECTORY + "/" + DIRECTORY_ROOT + "/" + hostn.replace("/",".")
+
+                    #CHANGE THE DIRECTORY TO THE ASSOSIATED HOST DIRECTORY
+                    os.chdir(WORKING_DIRECTORY + "/" + DIRECTORY_ROOT + "/" + hostn.replace("/","."))
+
+                    #KEEP A COPY OF THE HOST NAME BEFORE THE www. WAS REMOVED ; THIS WILL BE USED FOR THE GET REQUESTS 
+                    hostNameForRequest = fileNameAsStringArray[0]
+
+                    #FOR REQUESTS WITH WWW. RELATIVE PATH WILL BE THE INFORMATION AFTER THE FIRST SLASH
+                    relativePath = "/" + fileNameAsStringArray[2]
+                else:
+                    relativePath = "/" + filename
+
+                try:           
+                    #CREATE SOCKET FOR WEB SERVER CONNECTION
+                    c.connect((hostn,80))
+
+                    #CREATE MAKE FILE FOR SEND/RECV 
+                    fileobj = c.makefile('r', 0) 
+        
+                    #WRITE TO THE MAKEFILE TO SEND THE HTTP REQUEST TO THE WEB SERVER
+                    fileobj.write("GET {object} HTTP/1.0\r\n".format(object=relativePath))
+                    fileobj.write("Host: {host}\r\n\r\n".format(host=hostNameForRequest))
+
+                    #PRINT LINES TO THE TERMINAL 
+                    print("GET {object} HTTP/1.0".format(object=relativePath))
+                    print("Host: {host}\n".format(host=hostNameForRequest))
+ 
+                    #READ THE RESPONSE INTO THE BUFFER
+                    buffer = fileobj.readlines() 
+
+                    #CREATE A NEW FILE TO CACHE THE RESPONSE
+                    writeName = filename.replace("/",".")
+                    tmpFile = open("./" + writeName,"wb")
+                   
+                    #IF "Last-Modified:" IS NOT IN THE RESPONSE,USE THE CURRENT TIME 
+                    now = datetime.now()
+                    stamp = mktime(now.timetuple())
+                    dateLastMod =  format_date_time(stamp)
+
+                    #STREAM THE INFORMATION
+                    for m in buffer:
+
+                        #CHECK TO SEE IF LAST MODIFIED DATE WAS SENT
+                        if "Last-Modified" in m:
+                            dateLastMod = m.strip().split(' ', 1)[1]
+                        
+                        #WRITE TO CACHE FILE
+                        tmpFile.write(m)
+
+                        #SEND TO BROWSER
+                        clientSocket.send(m)
+
+                    #CLOSE THE CACHE FILE
+                    tmpFile.close()
+
+                    #ADD THIS INSTANCE TO THE INTERNAL DATASTRUCTURE
+                    if writeName not in cachedFiles.keys():
+
+                        #RECORD THE TIME THE FILE WAS CACHED
+                        now = datetime.now()
+                        stamp = mktime(now.timetuple())
+                        dateCached =  format_date_time(stamp)
+                        
+                        #INSERT THE INFORMATION INTO THE DICTIONARY 
+                        cachedFiles.update({writeName : (dateLastMod,dateCached,activePath,hostn,hostNameForRequest,relativePath)})
+
                 except:
                     print("Illegal request")
             else:
-                # HTTP response message for file not found
-                ########### #############################################################################
-                # Fill in start.
-                print '404: Not Found'
-                # Fill in end.
-                ########################################################################################
-        # Close the client and the server sockets
-        tcpCliSock.close()
-    # Fill in start.
+                # HTTP RESPONSE FOR IF NOT FOUND 
+                print ("404: Not Found")
+                response = "<html><body>" + "<h1>" + "404: Not Found" + "</h1>" +"</body></html>"
+                clientSocket.send("HTTP/1.0 404 NOT FOUND\r\n")
+                clientSocket.send("Content-Type:text/html\r\n\r\n")
+                clientSocket.send(response)
+
+        #CLOSE THE CLIENT SOCKET
+        clientSocket.close()
+    #CLOSE THE SERVER
     serverSocket.close()
-    # Fill in end.
 
 def establishServer():
+    #SOCKET DECLORATION
     serverSocket = socket(AF_INET, SOCK_STREAM)
     tcpSerPort = 8888  
     serverSocket.bind(("",tcpSerPort))
-    serverSocket.listen(5)
+    serverSocket.listen(10)
     return serverSocket
 
+def makeDirectory(dirName):
 
-
-
-def makeDirectory():
-    """
-    This function creates the folder the files will be saved
-    into as well as changes the working directory so the program 
-    will write into the new folder 
-    """
-    
-    dirName = 'Cache'
-
+    #IF THE DIRECTORY DOES NOT EXIST, CREATE IT
     try:
-
         os.mkdir(dirName)
         print(dirName + " directory created.")
 
     except OSError:
         print(dirName + " directory already exists")
-    
-    
-    os.chdir(dirName) #Change the operating directory to write files into this folder 
-    
 
 if __name__ == '__main__':
     main()
